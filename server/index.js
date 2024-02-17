650e41a989a419b428b66451ae6a76fb44e6da51
TohidEq <TohidEq@gmail.com>
2024-02-17 20:56:18 +0330

    Express js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,39 @@
-import { createServer } from "http"
-import { Server } from "socket.io"
+import express from "express";
+import { Server } from "socket.io";
 
-const httpServer = createServer()
+import path from "path";
+import { fileURLToPath } from "url";
 
-const io = new Server(httpServer, {
-    cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
-    }
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-io.on('connection', socket => {
-    console.log(`User ${socket.id} connected`)
+const PORT = process.env.PORT || 3500;
 
-    socket.on('message', data => {
-        console.log(data)
-        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
-    })
-})
+const app = express();
 
-httpServer.listen(3500, () => console.log('listening on port 3500'))
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+const io = new Server(expressServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? false
+        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
+  });
+});
+
+// httpServer.listen(3500, () => console.log("listening on port 3500"));
+
